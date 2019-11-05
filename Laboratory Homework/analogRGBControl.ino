35c897518e32f4242a0bb1a9ab0677c30922338c
DanutPanaite <57130892+DanutPanaite@users.noreply.github.com>
2019-11-05 14:44:48 +0200

    Add files via upload

---
+++ Laboratory Homework/analogRGBControl.ino
@@ -0,0 +1,36 @@
+const int redPin = 9;
+const int greenPin = 10;
+const int bluePin = 11;
+const int redPot = A0;
+const int greenPot = A1;
+const int bluePot = A2;
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redPot, INPUT);
+  pinMode(greenPot, INPUT);
+  pinMode(bluePot, INPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  redValue = map(analogRead(redPot), 0, 1023, 0, 255);
+  greenValue = map(analogRead(greenPot), 0, 1023, 0,255);
+  blueValue = map(analogRead(bluePot), 0, 1023, 0, 255);
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+  Serial.println(redValue);
+  Serial.println(greenValue);
+  Serial.println(blueValue);
+  delay(1000);
+
+  
+}
